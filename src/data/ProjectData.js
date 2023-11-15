f29903def57eadf482dca802a9c253830d5074fb
Brendan Regelbrugge <48809270+brendanreg@users.noreply.github.com>
2023-11-14 21:57:42 -0500

    Update ProjectData.js

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,7 +1,7 @@
 // --------------------------------------- Project List
 export const ProjectList = [
   {
-    img: "",
+    img: "https://github.com/brendanreg/Interactive_Portfolio/blob/main/src/Assets/get_rekt.png?raw=true",
     title: ".GET_REKT()",
     description:
       "Michigan State Python Boot Camp, coded a simple game in Python using pygame.",
